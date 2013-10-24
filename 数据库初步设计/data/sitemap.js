2e766421b98d616802ec0a7c1929034f1ebeed76
窦静轩 <dronaldo80@gmail.com>
2013-10-24 10:32:57 +0800

    添加数据库 简单设计

---
+++ 数据库初步设计/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s)])]);}; 
+var b="rootNodes",c="pageName",d="客户端数据库简单设计",e="type",f="Wireframe",g="url",h="客户端数据库简单设计.html",i="children",j="初步设计以及升级概要",k="初步设计以及升级概要.html",l="数据库文件位置",m="数据库文件位置.html",n="其他事项",o="其他事项.html",p="数据库结构",q="数据库结构.html",r="资源文件路径",s="资源文件路径.html";
+return _creator();
+})();
---
+++ 数据库初步设计/数据库文件位置_files/axurerp_pagespecificscript.js
@@ -0,0 +1,51 @@
+﻿for(var i = 0; i < 35; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u21'] = 'center';gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u25'] = 'center';document.getElementById('u27_img').tabIndex = 0;
+
+u27.style.cursor = 'pointer';
+$axure.eventManager.click('u27', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('数据库结构.html');
+
+}
+});
+gv_vAlignTable['u28'] = 'center';document.getElementById('u31_img').tabIndex = 0;
+
+u31.style.cursor = 'pointer';
+$axure.eventManager.click('u31', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('数据库结构.html');
+
+}
+});
+gv_vAlignTable['u32'] = 'center';document.getElementById('u33_img').tabIndex = 0;
+
+u33.style.cursor = 'pointer';
+$axure.eventManager.click('u33', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('数据库结构.html');
+
+}
+});
+gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u8'] = 'center';document.getElementById('u10_img').tabIndex = 0;
+
+u10.style.cursor = 'pointer';
+$axure.eventManager.click('u10', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('资源文件路径.html');
+
+}
+});
+gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';
